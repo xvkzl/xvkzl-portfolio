0d3f6d8ea93831227ca0851e4975946555898030
羅神 <277219248+xvkzl@users.noreply.github.com>
2026-05-31 23:29:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    base: '/xvkzl-portfolio/'
   ],
 })
