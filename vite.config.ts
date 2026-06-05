1c060b94a62a2a650642ca525b78ab58ef8dfa89
羅神 <277219248+xvkzl@users.noreply.github.com>
2026-06-05 10:53:41 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/xvkzl-portfolio/',
+  base: '/',
 
   plugins: [
     react(),
